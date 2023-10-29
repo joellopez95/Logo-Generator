655e746095851c0e33b2cabd5d5880d2e822f4e5
Joel Lopez <jolopez.satx@gmail.com>
2023-10-28 20:41:35 -0500

    stored user input code, svg cont generator code

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+//import lib and inquire
 const fs = require('fs');
 const { Circle, Triangle, Square } = require('./lib/shapes');
 const inquirer = require('inquirer')
 
+//function to get user input
 const getUserInput = (questions) => {
     return inquirer.prompt(questions);
 };
 
+//function to generate the logo
 const generateLogo = ()=> {
     let text, textColor, selectedShape, shapeColor;
-}
 
+//start process of obtaining user info
 getUserInput([
     {
         type: 'input',
@@ -22,16 +25,81 @@ getUserInput([
         message: 'Enter textColor',
     },
     {
-        type: 'checkbox',
+        type: 'input',
         name: 'shapeChoice',
-        message: 'Choose shape',
-        Choices: [
-            { name: 'Circle', value: '1' },
-            { name: 'Triangle', value: '2' },
-            { name: 'Square', value: '3' }
-        ]
+        message: 'Choose a shape - 1 for Circle, 2 for Triangle, 3 for Square:',
     }
 ])
+
+    .then((answers) => {
+        //store user inputs
+        text = answers.text;
+        textColor = answers.textColor;
+        const shapeChoices = answers.shapeChoice;
+        //check selected shape
+        if (shapeChoices === '1') {
+            selectedShape = new Circle(50);
+        }
+        if (shapeChoices === '2') {
+            selectedShape = new Triangle();
+        }
+        if (shapeChoices === '3') {
+           return getUserInput([
+            {
+                type:'input',
+                name: 'sideLength',
+                message: 'Enter side length of square:',
+            }
+           ]);
+        } else {
+            console.log('Invalid, default Circle.');
+            selectedShape = new Circle(50);
+        //default to circle
+        return null;
+        }
+    })
+    .then((result) => {
+        //create square if selected
+        if (result) {
+            selectedShape = new Square(parseInt(result.sideLength, 10) || 50);
+        }
+        return getUserInput([
+            {
+                type: 'input',
+                name: 'shapeColor',
+                message: 'Enter Shape color:'
+            }
+        ]);
+    })
+
+    .then((answers) => {
+        //store shape color
+        shapeColor = answers.shapeColor;
+        //svg cont
+        const svgContent = `
+        <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+          ${selectedShape.getSVG()}
+          <text x="100" y="150" fill="${textColor}">${text}</text>
+        </svg>
+      `;
+        //writes svg cont to examples folder
+        fs.writeFile('examples/logo.svg', svgContent, (err) => {
+          if (err) {
+            throw err;//error
+          } else {
+            console.log('Generated logo.svg'); //success!
+          }
+        });
+      })
+      .catch((err) => {
+        throw err;//error
+      });
+};
+  
+  //starts process
+  generateLogo();
+    
+
 //I WANT to generate a simple logo for my projects
 //SO THAT I don't have to pay a graphic designer
 //Acceptance Criteria
